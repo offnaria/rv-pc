ae6f3bbad6285cc308bd905ba1690c57b3640f7e
offnaria <yamada@arch.cs.titech.ac.jp>
2024-08-09 05:21:23 +0900

    WIP: Add PLIC module.

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -117,6 +117,7 @@ set files [list \
  [file normalize "${origin_dir}/src/sources_1/rtl/interconnect.v" ]\
  [file normalize "${origin_dir}/src/sources_1/rtl/mouse.v" ]\
  [file normalize "${origin_dir}/src/sources_1/rtl/periph_sdcard.v" ]\
+ [file normalize "${origin_dir}/src/sources_1/rtl/plic.v" ]\
  [file normalize "${origin_dir}/src/sources_1/rtl/ps2interface.v" ]\
  [file normalize "${origin_dir}/src/sources_1/rtl/rvcluster.v" ]\
  [file normalize "${origin_dir}/src/sources_1/rtl/rvcorepl-smp.v" ]\
