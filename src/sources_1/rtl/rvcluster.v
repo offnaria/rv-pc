6e3e47570a9bb0d63cd0c5ea8de7d91550dfe982
offnaria <yamada@arch.cs.titech.ac.jp>
2024-09-02 21:11:23 +0900

    Rename signals.

--- src/sources_1/rtl/rvcluster.v
+++ src/sources_1/rtl/rvcluster.v
@@ -40,35 +40,39 @@ module m_RVCluster #(
     output wire               w_cluster_tlb_acs
 );
 
-    wire [31:0] w_pagefault;
-    wire [31:0] w_mstatus;
-    wire [1:0]  w_tlb_req;
-    wire        w_tlb_flush;
-    wire        w_iscode;
-    wire        w_isread;
-    wire        w_iswrite;
-    wire        w_pte_we;
-    wire [31:0] w_pte_wdata;
-    wire        w_use_tlb;
-    wire        w_tlb_hit;
-    wire  [2:0] w_pw_state;
-    wire        w_tlb_busy;
-    wire [31:0] w_tlb_addr;
-    wire  [2:0] w_tlb_use;
-    wire [31:0] w_tlb_pte_addr;
-    wire        w_tlb_acs;
-
-    wire [31:0] w_priv;
-    wire [31:0] w_satp;
-    wire [31:0] w_insn_addr;
-    wire [31:0] w_data_addr;
-    wire w_core_is_paddr = (w_priv == `PRIV_M) || (w_satp[31] == 0);
-    wire [31:0] w_core_iaddr = (w_core_is_paddr) ? w_insn_addr : w_tlb_addr;
-    wire [31:0] w_core_daddr = (w_core_is_paddr) ? w_data_addr : w_tlb_addr;
+    wire [31:0] w_core_iaddr;
+    wire [31:0] w_core_daddr;
     wire [31:0] w_core_data_wdata;
     wire [2:0]  w_core_data_ctrl;
     wire        w_core_init_stage;
     wire        w_core_data_we;
+    wire        w_core_is_paddr;
+    wire        w_core_iscode;
+    wire        w_core_isread;
+    wire        w_core_iswrite;
+    wire        w_core_pte_we;
+    wire [31:0] w_core_pte_wdata;
+    wire        w_core_use_tlb;
+    wire        w_core_tlb_hit;
+    wire  [2:0] w_core_pw_state;
+    wire        w_core_tlb_busy;
+    wire  [2:0] w_core_tlb_use;
+    wire [31:0] w_core_tlb_pte_addr;
+    wire        w_core_tlb_acs;
+
+    wire [31:0] w_pagefault;
+    wire [31:0] w_mstatus;
+    wire [1:0]  w_tlb_req;
+    wire        w_tlb_flush;
+    wire [31:0] w_priv;
+    wire [31:0] w_satp;
+    wire [31:0] w_insn_addr;
+    wire [31:0] w_data_addr;
+    wire [31:0] w_tlb_addr;
+
+    assign w_core_is_paddr = (w_priv == `PRIV_M) || (w_satp[31] == 0);
+    assign w_core_iaddr = (w_core_is_paddr) ? w_insn_addr : w_tlb_addr;
+    assign w_core_daddr = (w_core_is_paddr) ? w_data_addr : w_tlb_addr;
 
     m_RVCorePL_SMP #(
         .MHARTID(0)
@@ -114,20 +118,20 @@ module m_RVCluster #(
         .w_dram_odata(w_dram_odata),
         .w_tlb_flush(w_tlb_flush),
         .w_mode_is_cpu(w_mode_is_cpu),
-        .w_iscode(w_iscode),
-        .w_isread(w_isread),
-        .w_iswrite(w_iswrite),
-        .w_pte_we(w_pte_we),
-        .w_pte_wdata(w_pte_wdata),
+        .w_iscode(w_core_iscode),
+        .w_isread(w_core_isread),
+        .w_iswrite(w_core_iswrite),
+        .w_pte_we(w_core_pte_we),
+        .w_pte_wdata(w_core_pte_wdata),
         .w_pagefault(w_pagefault),
-        .w_use_tlb(w_use_tlb),
-        .w_tlb_hit(w_tlb_hit),
-        .w_pw_state(w_pw_state),
-        .w_tlb_busy(w_tlb_busy),
+        .w_use_tlb(w_core_use_tlb),
+        .w_tlb_hit(w_core_tlb_hit),
+        .w_pw_state(w_core_pw_state),
+        .w_tlb_busy(w_core_tlb_busy),
         .w_tlb_addr(w_tlb_addr),
-        .w_tlb_use(w_tlb_use),
-        .w_tlb_pte_addr(w_tlb_pte_addr),
-        .w_tlb_acs(w_tlb_acs)
+        .w_tlb_use(w_core_tlb_use),
+        .w_tlb_pte_addr(w_core_tlb_pte_addr),
+        .w_tlb_acs(w_core_tlb_acs)
     );
 
     assign w_cluster_iaddr = w_core_iaddr;
@@ -137,16 +141,16 @@ module m_RVCluster #(
     assign w_cluster_init_stage = w_core_init_stage;
     assign w_cluster_data_we = w_core_data_we;
     assign w_cluster_is_paddr = w_core_is_paddr;
-    assign w_cluster_iscode = w_iscode;
-    assign w_cluster_isread = w_isread;
-    assign w_cluster_iswrite = w_iswrite;
-    assign w_cluster_pte_we = w_pte_we;
-    assign w_cluster_pte_wdata = w_pte_wdata;
-    assign w_cluster_use_tlb = w_use_tlb;
-    assign w_cluster_tlb_hit = w_tlb_hit;
-    assign w_cluster_pw_state = w_pw_state;
-    assign w_cluster_tlb_busy = w_tlb_busy;
-    assign w_cluster_tlb_use = w_tlb_use;
-    assign w_cluster_tlb_pte_addr = w_tlb_pte_addr;
-    assign w_cluster_tlb_acs = w_tlb_acs;
+    assign w_cluster_iscode = w_core_iscode;
+    assign w_cluster_isread = w_core_isread;
+    assign w_cluster_iswrite = w_core_iswrite;
+    assign w_cluster_pte_we = w_core_pte_we;
+    assign w_cluster_pte_wdata = w_core_pte_wdata;
+    assign w_cluster_use_tlb = w_core_use_tlb;
+    assign w_cluster_tlb_hit = w_core_tlb_hit;
+    assign w_cluster_pw_state = w_core_pw_state;
+    assign w_cluster_tlb_busy = w_core_tlb_busy;
+    assign w_cluster_tlb_use = w_core_tlb_use;
+    assign w_cluster_tlb_pte_addr = w_core_tlb_pte_addr;
+    assign w_cluster_tlb_acs = w_core_tlb_acs;
 endmodule
