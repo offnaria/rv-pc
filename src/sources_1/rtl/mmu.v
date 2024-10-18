7d2dda7657fe9c5bd5f214cdca3b27924799c305
offnaria <yamada@arch.cs.titech.ac.jp>
2024-10-18 19:55:33 +0900

    Migrate two data TLBs into one, also some naming duplications are solved. However, Linux doesn't boot. It might be because we don't check the write permission in the data TLB.

--- src/sources_1/rtl/mmu.v
+++ src/sources_1/rtl/mmu.v
@@ -102,13 +102,12 @@ module m_mmu (
     reg   [2:0] r_tlb_usage  = 0;
     assign w_tlb_addr = r_tlb_addr;
     assign w_tlb_usage  = r_tlb_usage;
-    wire [21:0] w_tlb_inst_addr, w_tlb_data_addr, w_tlb_data_w_addr;
-    wire        w_tlb_inst_hit, w_tlb_data_hit, w_tlb_data_w_oe;
+    wire [21:0] w_tlb_inst_addr, w_tlb_data_addr;
+    wire        w_tlb_inst_hit, w_tlb_data_hit;
     assign w_use_tlb = (w_mode_is_cpu && (w_iscode || w_isread || w_iswrite)
                                           && (!(w_priv == `PRIV_M || w_satp[31] == 0)));
     assign w_tlb_hit = ((w_iscode && w_tlb_inst_hit) ||
-                            (w_isread && w_tlb_data_hit)  ||
-                            (w_iswrite && w_tlb_data_w_oe));
+                            ((w_isread || w_iswrite) && w_tlb_data_hit));
     assign w_pw_done = (r_pw_state == 7);
 
     // PAGE WALK state
@@ -124,12 +123,7 @@ module m_mmu (
                 else begin
                     r_pw_state <= 7;
                     r_tlb_busy <= 1;
-                    case ({w_iscode, w_isread, w_iswrite})
-                        3'b100 : r_tlb_addr <= {w_tlb_inst_addr[21:2], w_insn_addr[11:0]};
-                        3'b010 : r_tlb_addr <= {w_tlb_data_addr[21:2], w_data_addr[11:0]};
-                        3'b001 : r_tlb_addr <= {w_tlb_data_w_addr[21:2], w_data_addr[11:0]};
-                        default: r_tlb_addr <= 0;
-                    endcase
+                    r_tlb_addr <= (w_iscode) ? w_tlb_inst_addr : (w_isread || w_iswrite) ? w_tlb_data_addr : 0;
                     r_tlb_usage <= {w_iscode, w_isread, w_iswrite};
                 end
                 {r_iscode, r_isread, r_iswrite} <= {w_iscode, w_isread, w_iswrite};
@@ -189,17 +183,16 @@ module m_mmu (
     /***********************************           TLB          ***********************************/
     wire        w_tlb_inst_we   = (r_pw_state == 5 && !page_walk_fail && w_iscode);
     wire        w_tlb_data_we   = (r_pw_state == 5 && !page_walk_fail && !w_iscode);
-    wire        w_tlb_data_w_we   = (r_pw_state == 5 && !page_walk_fail && w_iswrite);
     wire [21:0] w_tlb_wdata       = {physical_addr[31:12], 2'b0};
 
     wire [PPN_WIDTH:0] w_ppn = w_satp[21:0];
-    wire [TLB_ADDR_WIDTH-1:0] w_tlb_inst_addr = {w_ppn, w_insn_addr[31:12]};
-    wire [TLB_ADDR_WIDTH-1:0] w_tlb_data_addr = {w_ppn, w_data_addr[31:12]};
+    wire [TLB_ADDR_WIDTH-1:0] w_tlb_inst_rwaddr = {w_ppn, w_insn_addr[31:12]};
+    wire [TLB_ADDR_WIDTH-1:0] w_tlb_data_rwaddr = {w_ppn, w_data_addr[31:12]};
 
     generate
         if (ENABLE_ITLB) begin
             m_cache_dmap#(TLB_ADDR_WIDTH, TLB_DATA_WIDTH, TLB_ENTRY)
-            TLB_inst_r (CLK, 1'b1, w_tlb_flush, w_tlb_inst_we, w_tlb_inst_addr, w_tlb_inst_addr, w_tlb_wdata, w_tlb_inst_addr, w_tlb_inst_hit);
+            TLB_inst_r (CLK, 1'b1, w_tlb_flush, w_tlb_inst_we, w_tlb_inst_rwaddr, w_tlb_inst_rwaddr, w_tlb_wdata, w_tlb_inst_addr, w_tlb_inst_hit);
         end else begin
             assign w_tlb_inst_addr = 0;
             assign w_tlb_inst_hit = 0;
@@ -209,20 +202,10 @@ module m_mmu (
     generate
         if (ENABLE_RTLB) begin
             m_cache_dmap#(TLB_ADDR_WIDTH, TLB_DATA_WIDTH, TLB_ENTRY)
-            TLB_data_r (CLK, 1'b1, w_tlb_flush, w_tlb_data_we, w_tlb_data_addr, w_tlb_data_addr, w_tlb_wdata, w_tlb_data_addr, w_tlb_data_hit);
+            TLB_data_r (CLK, 1'b1, w_tlb_flush, w_tlb_data_we, w_tlb_data_rwaddr, w_tlb_data_rwaddr, w_tlb_wdata, w_tlb_data_addr, w_tlb_data_hit);
         end else begin
             assign w_tlb_data_addr = 0;
             assign w_tlb_data_hit = 0;
-        end
-    endgenerate
-
-    generate
-        if (ENABLE_WTLB) begin
-            m_cache_dmap#(TLB_ADDR_WIDTH, TLB_DATA_WIDTH, TLB_ENTRY)
-            TLB_data_w (CLK, 1'b1, w_tlb_flush, w_tlb_data_w_we, w_tlb_data_addr, w_tlb_data_addr, w_tlb_wdata, w_tlb_data_w_addr, w_tlb_data_w_oe);
-        end else begin
-            assign w_tlb_data_w_addr = 0;
-            assign w_tlb_data_w_oe = 0;
         end
     endgenerate
 
