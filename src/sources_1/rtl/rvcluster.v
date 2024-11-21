7b6ebefbd256ae55c4da3e26f4744e85f6169bf8
offnaria <yamada@arch.cs.titech.ac.jp>
2024-11-21 19:38:13 +0900

    Update the arbiter to assert the busy signal only when a memory access is sent. However, an instruction fetch failes. It is because the MMU doesn't work when the microcontroller is active, but the busy signal is deasserted, so the core regards it as the request completed.  Our next task is taking care of the microcontroller.

--- src/sources_1/rtl/rvcluster.v
+++ src/sources_1/rtl/rvcluster.v
@@ -36,13 +36,44 @@ module m_RVCluster #(
     wire [2:0]  w_core_mem_ctrl     [0:N_HARTS-1];
     wire        w_core_dram_re      [0:N_HARTS-1];
 
-    assign w_cluster_data_wdata = w_core_data_wdata[r_hart_sel];
-    assign w_cluster_init_stage = w_core_init_stage[r_hart_sel];
-    assign w_cluster_data_we    = w_core_data_we[r_hart_sel];
-    assign w_cluster_dev_addr   = w_core_dev_addr[r_hart_sel];
-    assign w_cluster_dram_addr  = w_core_dram_addr[r_hart_sel];
-    assign w_cluster_mem_ctrl   = w_core_mem_ctrl[r_hart_sel];
-    assign w_cluster_dram_re    = w_core_dram_re[r_hart_sel];
+    reg [31:0] r_core_data_wdata   [0:N_HARTS-1];
+    reg        r_core_init_stage   [0:N_HARTS-1];
+    reg        r_core_data_we      [0:N_HARTS-1];
+    reg [31:0] r_core_dev_addr     [0:N_HARTS-1];
+    reg [31:0] r_core_dram_addr    [0:N_HARTS-1];
+    reg [2:0]  r_core_mem_ctrl     [0:N_HARTS-1];
+    reg        r_core_dram_re      [0:N_HARTS-1];
+
+    integer i;
+    always @(posedge CLK) begin
+        for (i = 0; i < N_HARTS; i = i + 1) begin
+            if (!RST_X) begin
+                r_core_data_wdata[i] <= 0;
+                r_core_init_stage[i] <= 0;
+                r_core_data_we[i]    <= 0;
+                r_core_dev_addr[i]   <= 0;
+                r_core_dram_addr[i]  <= 0;
+                r_core_mem_ctrl[i]   <= 0;
+                r_core_dram_re[i]    <= 0;
+            end else begin
+                r_core_data_wdata[i] <= (w_core_data_we[i]) ? w_core_data_wdata[i] : r_core_data_wdata[i];
+                r_core_init_stage[i] <= w_core_init_stage[i];
+                r_core_data_we[i]    <= (w_arbiter_done[i]) ? 0 : (w_core_data_we[i]) ? 1 : r_core_data_we[i];
+                r_core_dev_addr[i]   <= (w_core_data_we[i] || w_core_dram_re[i]) ? w_core_dev_addr[i] : r_core_dev_addr[i];
+                r_core_dram_addr[i]  <= (w_core_data_we[i] || w_core_dram_re[i]) ? w_core_dram_addr[i] : r_core_dram_addr[i];
+                r_core_mem_ctrl[i]   <= (w_core_data_we[i] || w_core_dram_re[i]) ? w_core_mem_ctrl[i] : r_core_mem_ctrl[i];
+                r_core_dram_re[i]    <= (w_arbiter_done[i]) ? 0 : (w_core_dram_re[i]) ? 1 : r_core_dram_re[i];
+            end
+        end
+    end
+
+    assign w_cluster_data_wdata = r_core_data_wdata[r_hart_sel];
+    assign w_cluster_init_stage = r_core_init_stage[r_hart_sel];
+    assign w_cluster_data_we    = r_core_data_we[r_hart_sel] && !w_arbiter_done[r_hart_sel]; // TODO
+    assign w_cluster_dev_addr   = r_core_dev_addr[r_hart_sel];
+    assign w_cluster_dram_addr  = r_core_dram_addr[r_hart_sel];
+    assign w_cluster_mem_ctrl   = r_core_mem_ctrl[r_hart_sel];
+    assign w_cluster_dram_re    = r_core_dram_re[r_hart_sel] && !w_arbiter_done[r_hart_sel]; // TODO
 
     wire [N_HARTS-1:0] w_core_busy;
     wire [N_HARTS-1:0] w_core_next_state_is_idle;
@@ -94,8 +125,29 @@ module m_RVCluster #(
     endgenerate
 
     /****************************** Cluster Arbiter ******************************/
+    wire [N_HARTS-1:0] w_arbiter_request;
+    wire [N_HARTS-1:0] w_arbiter_selected;
+    wire [N_HARTS-1:0] w_arbiter_done;
+    localparam S_IDLE = 0;
+    localparam S_PENDING = 1;
+    localparam S_WAITING = 2;
+    reg [1:0] r_arbiter_core_state [0:N_HARTS-1];
+    always @(posedge CLK) begin
+        for (i = 0; i < N_HARTS; i = i + 1) begin
+            if (!RST_X) begin
+                r_arbiter_core_state[i] <= S_IDLE;
+            end else begin
+                case (r_arbiter_core_state[i])
+                    S_IDLE: if (w_arbiter_request[i]) r_arbiter_core_state[i] <= S_PENDING;
+                    S_PENDING: if (w_arbiter_selected[i]) r_arbiter_core_state[i] <= S_WAITING;
+                    S_WAITING: if (w_arbiter_done[i]) r_arbiter_core_state[i] <= S_IDLE;
+                endcase
+            end
+        end
+    end
+
     reg [$clog2(N_HARTS+1)-1:0] r_hart_sel;
-    wire w_hart_sel_changable = !w_next_mode_is_mc && w_mode_is_cpu && w_core_next_state_is_idle[r_hart_sel] && w_core_interrupt_ok[r_hart_sel] && w_core_tkn[r_hart_sel] && !w_core_take_exception[r_hart_sel] && !w_core_page_walk_fail[r_hart_sel] && !w_core_csr_flush[r_hart_sel] && !w_core_tlb_flush[r_hart_sel];
+    wire w_hart_sel_changable = (r_arbiter_core_state[r_hart_sel] == S_IDLE);
     always @ (posedge CLK) begin
         if (!RST_X) begin
             r_hart_sel <= 0;
@@ -105,7 +157,10 @@ module m_RVCluster #(
     end
 
     for (g = 0; g < N_HARTS; g = g + 1) begin
-        assign w_core_busy[g] = (r_hart_sel == g) ? w_interconnect_busy : 1;
+        assign w_arbiter_request[g] = w_core_data_we[g] || w_core_dram_re[g];
+        assign w_arbiter_selected[g] = (r_hart_sel == g);
+        assign w_arbiter_done[g] = (r_arbiter_core_state[g] == S_WAITING) && !w_interconnect_busy;
+        assign w_core_busy[g] = (r_arbiter_core_state[g] == S_PENDING) || ((r_arbiter_core_state[g] == S_WAITING) && !w_arbiter_done[g]); // TODO
     end
 
 endmodule
